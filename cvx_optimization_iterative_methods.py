6506588c76dfdc47f91471e031dd68b6f5a7b1a1
Baha2r <baha2r.mirzazadeh98@gmail.com>
2021-01-01 13:28:55 +0330

    Optimizing the code...

--- cvx_optimization_iterative_methods.py
+++ cvx_optimization_iterative_methods.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from sympy import *
+from sympy import Symbol, parse_expr
 import sympy as sym
 import numpy as np
 
@@ -7,28 +7,13 @@ import numpy as np
 class IterativeMethods(ABC):
     _sym_symbols = list()
     _function = None
+    _n = None
 
     @abstractmethod
-    def _assign_symbols(self, symbol: list) -> None:
-        pass
-
-    @abstractmethod
-    def _check_function_syntax(self, func: str) -> None:
-        pass
-
-    @abstractmethod
-    def _gradient(self, point: np.ndarray) -> np.ndarray:
-        pass
-
-
-class GradientDescentWithLinearSearch(IterativeMethods):
-    __n = None
-    iterations = 0
-
     def __init__(self, symbols_list: list, function: str) -> None:
         self._assign_symbols(symbols_list)
         self._check_function_syntax(function)
-        self.__n = self._sym_symbols.__len__()
+        self._n = self._sym_symbols.__len__()
 
     def _assign_symbols(self, symbol: list) -> None:
         for i in range(0, symbol.__len__()):
@@ -45,9 +30,9 @@ class GradientDescentWithLinearSearch(IterativeMethods):
         for i in range(0, point.size):
             symbol_vars[self._sym_symbols[i]] = point[i]
 
-        gradient = np.zeros((self.__n,), dtype=float)
+        gradient = np.zeros((self._n,), dtype=float)
 
-        for i in range(0, self.__n):
+        for i in range(0, self._n):
             temp_gradient = sym.diff(self._function, self._sym_symbols[i]).evalf(subs=symbol_vars)
             if type(temp_gradient) == sym.core.mul.Mul:
                 raise SyntaxError('The function contains unknown tokens.')
@@ -55,18 +40,25 @@ class GradientDescentWithLinearSearch(IterativeMethods):
 
         return gradient
 
-    def gradient_descent(self, first_point: list, step_size: float, epsilon: float = 0.0001):
-        if first_point.__len__() != self.__n:
+
+class GradientDescentWithLinearSearch(IterativeMethods):
+    iterations = 0
+
+    def __init__(self, symbols_list: list, function: str) -> None:
+        super().__init__(symbols_list, function)
+
+    def gradient_descent(self, first_point: list, step_size: float, epsilon: float = 0.0001) -> dict:
+        if first_point.__len__() != self._n:
             raise IndexError('Mismatched first point ' + str(first_point) + ' with variables ' + str(self._sym_symbols))
 
         iterative_point = np.array(first_point)
-        gradient_vector = self._gradient(iterative_point) * -1
+        gradient_vector = self._gradient(iterative_point)
         self.iterations = 0
         result = {}
 
         while np.linalg.norm(gradient_vector) > epsilon:
-            iterative_point = np.add(iterative_point, step_size * gradient_vector)
-            gradient_vector = self._gradient(iterative_point) * -1
+            iterative_point = np.subtract(iterative_point, step_size * gradient_vector)
+            gradient_vector = self._gradient(iterative_point)
             self.iterations += 1
             # print('Optimal point: ', iterative_point, '\t', 'Iterations: ', self.iterations)
 
@@ -74,11 +66,13 @@ class GradientDescentWithLinearSearch(IterativeMethods):
         return result
 
     class NewtonMethod(IterativeMethods):
-        def _gradient(self, point: np.ndarray) -> np.ndarray:
+        iterations = 0
+
+        def __init__(self, symbols_list: list, function: str) -> None:
+            super().__init__(symbols_list, function)
+
+        def __hessian(self, point: np.ndarray) -> np.ndarray:
             pass
 
-        def _check_function_syntax(self, func: str) -> None:
-            pass
-
-        def _assign_symbols(self, symbol: list) -> None:
+        def newton_opt(self, first_point: list) -> dict:
             pass
