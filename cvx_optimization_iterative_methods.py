Some bugs fixed in gradient descent etc...

cvx_optimization_iterative_methods.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from sympy import Symbol, parse_expr, solve
+from sympy import Symbol, parse_expr, solveset, S
 import sympy as sym
 import numpy as np
 import numpy.linalg as linear_algebra
@@ -27,7 +27,7 @@ class IterativeMethods(ABC):
             raise SyntaxError('Syntax error in function: ' + func)
 
     def _gradient(self, point: np.ndarray) -> np.ndarray:
-        symbol_value = {}
+        symbol_value = dict()
         for i in range(0, point.size):
             symbol_value[self._sym_symbols[i]] = point[i]
 
@@ -47,14 +47,14 @@ class IterativeMethods(ABC):
         self._n = None
 
 
-class GradientDescentWithLinearSearch(IterativeMethods):
+class GradientDescentWithLineSearch(IterativeMethods):
     iterations = 0
 
     def __init__(self, symbols_list: list, function: str) -> None:
         self._reset()
         super().__init__(symbols_list, function)
 
-    def __step_size_linear_search(self, x_point: list, d_point: list) -> float:
+    def __step_size_line_search(self, x_point: list, d_point: list) -> float:
         temp = list()
         for i in range(0, self._n):
             d_point[i] = str(d_point[i]) + '*t'
@@ -69,38 +69,42 @@ class GradientDescentWithLinearSearch(IterativeMethods):
             if temp_func.__contains__(s):
                 temp_func = temp_func.replace(s, '(' + temp[i] + ')')
 
-        answers = solve(sym.diff(parse_expr(temp_func), Symbol('t')))
+        t = Symbol('t')
+        h = sym.diff(parse_expr(temp_func), t)
 
-        for answer in answers:
-            try:
-                simplified_answer = float(answer)
-                if type(simplified_answer) == float:
-                    return simplified_answer
-            except TypeError:
-                pass
+        try:
+            answers = solveset(h, t, domain=S.Reals)
+        except Exception:
+            raise ValueError('Convergence to root failed.')
 
-        raise ValueError('Unacceptable complex root in finding step size.')
+        if type(answers) != sym.sets.sets.FiniteSet:
+            raise ValueError(
+                'Unacceptable root in finding step size. The equation: '
+                + '\"' + str(h) + '\"' + ' has no root in Real numbers system.')
 
-    def gradient_descent(self, first_point: list, epsilon: float = 0.000000001) -> dict:
+        return float(answers.args[0])
+
+    def gradient_descent(self, first_point: list, epsilon: float = 0.0000001) -> None:
         if first_point.__len__() != self._n:
             raise IndexError('Mismatched first point ' + str(first_point) + ' with variables ' + str(self._sym_symbols))
 
         iterative_point = np.array(first_point)
         gradient_vector = self._gradient(iterative_point)
         self.iterations = 0
-        result = {}
 
         while linear_algebra.norm(gradient_vector) > epsilon:
-            step_size = self.__step_size_linear_search(list(iterative_point), list(gradient_vector * -1))
+            step_size = self.__step_size_line_search(list(iterative_point), list(gradient_vector * -1))
             iterative_point = np.subtract(iterative_point, step_size * gradient_vector)
             gradient_vector = self._gradient(iterative_point)
             self.iterations += 1
-            print('Optimal point: ', iterative_point, '\t', 'Iterations: ', self.iterations, '\t', 'Step Size: ',
+            print('Optimal point: ', iterative_point, '\t', 'In iteration: ', self.iterations, '\t', 'Step Size: ',
                   step_size)
 
-        result.update({'Optimal point': iterative_point, 'Iterations': self.iterations})
-        self._reset()
-        return result
+        opt_symbol_value = dict()
+        for i in range(0, self._n):
+            opt_symbol_value[self._sym_symbols[i]] = iterative_point[i]
+
+        print('Optimal value: ' + str(self._function.evalf(subs=opt_symbol_value)))
 
 
 class NewtonMethod(IterativeMethods):
@@ -111,7 +115,7 @@ class NewtonMethod(IterativeMethods):
         super().__init__(symbols_list, function)
 
     def __hessian(self, point: np.ndarray) -> np.ndarray:
-        symbol_vars = {}
+        symbol_vars = dict()
         for i in range(0, point.size):
             symbol_vars[self._sym_symbols[i]] = point[i]
 
@@ -130,21 +134,23 @@ class NewtonMethod(IterativeMethods):
 
         return hessian_matrix
 
-    def newton_opt(self, first_point: list, epsilon: float = 0.000000001) -> dict:
+    def newton_opt(self, first_point: list, epsilon: float = 0.00000001) -> None:
         if first_point.__len__() != self._n:
             raise IndexError('Mismatched first point ' + str(first_point) + ' with variables ' + str(self._sym_symbols))
 
         iterative_point = np.array(first_point)
         self.iterations = 0
-        result = {}
 
         while True:
             t = np.matmul(linear_algebra.inv(self.__hessian(iterative_point)), self._gradient(iterative_point))
             iterative_point = np.subtract(iterative_point, t)
             self.iterations += 1
-            print('Optimal point: ', iterative_point, '\t', 'Iterations: ', self.iterations)
+            print('Optimal point: ', iterative_point, '\t', 'In iteration: ', self.iterations)
             if linear_algebra.norm(t) < epsilon:
                 break
 
-        result.update({'Optimal point': iterative_point, 'Iterations': self.iterations})
-        return result
+        opt_symbol_value = dict()
+        for i in range(0, self._n):
+            opt_symbol_value[self._sym_symbols[i]] = iterative_point[i]
+
+        print('Optimal value: ' + str(self._function.evalf(subs=opt_symbol_value)))